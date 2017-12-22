ca9eb85d3905d6b57640557a469e7c0028977213
Cai <zw.cai@mediatek.com>
2017-12-22 14:23:34 +0800

    Implement HealthKit API.

--- HealthKit API Demo/ViewController.m
+++ HealthKit API Demo/ViewController.m
@@ -7,23 +7,358 @@
 //
 
 #import "ViewController.h"
+#import <HealthKit/HealthKit.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+@property (strong, nonatomic) HKHealthStore *healthStore;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _button.layer.borderColor = _button.tintColor.CGColor;
+    _button.layer.borderWidth = 1.f;
+    _button.layer.cornerRadius = 4.f;
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self getHealthKit];
 }
 
+#pragma mark - HealthKit Store
+
+- (void)getHealthKit
+{
+    if (NSClassFromString(@"HKHealthStore") &&
+        [HKHealthStore isHealthDataAvailable])
+    {
+        _healthStore = [HKHealthStore new];
+        
+        NSSet *shareTypes
+        = [NSSet setWithObjects:
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyMass],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeight],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyMassIndex],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyFatPercentage],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodPressureSystolic],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodPressureDiastolic],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRate],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRateVariabilitySDNN],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount],
+           [HKObjectType characteristicTypeForIdentifier:HKCategoryTypeIdentifierSleepAnalysis],
+           [HKObjectType characteristicTypeForIdentifier:HKCategoryTypeIdentifierAppleStandHour],
+           nil];
+        
+        NSSet *readTypes
+        = [NSSet setWithObjects:
+           [HKObjectType characteristicTypeForIdentifier:HKCharacteristicTypeIdentifierDateOfBirth],
+           [HKObjectType characteristicTypeForIdentifier:HKCharacteristicTypeIdentifierBiologicalSex],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyMass],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeight],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyMassIndex],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyFatPercentage],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodPressureSystolic],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodPressureDiastolic],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRate],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRateVariabilitySDNN],
+           [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount],
+           [HKObjectType characteristicTypeForIdentifier:HKCategoryTypeIdentifierAppleStandHour],
+           [HKObjectType characteristicTypeForIdentifier:HKCategoryTypeIdentifierSleepAnalysis],
+           nil];
+        
+        [_healthStore requestAuthorizationToShareTypes:shareTypes
+                                             readTypes:readTypes
+                                            completion:^(BOOL success,
+                                                         NSError *error)
+         {
+             
+             if (success) {
+                 // 'success' here means we did successfully ask for
+                 // authorization, does not mean we were authorized!
+                 [self clearTextView];
+                 [self getBirthday];
+                 [self getBiologicalSex];
+                 [self getHeight];
+                 [self getWeight];
+                 [self getStepCount];
+                 [self getHeartRate];
+             } else if (error) {
+                 [self addText:[error localizedDescription]];
+             }
+         }];
+    } else {
+        [self clearTextView];
+        [self addText:@"HealthKit is not available."];
+    }
+}
+
+- (BOOL)getWriteAuthorizationStatusForType:(HKObjectType *)type
+                                     label:(NSString *)label
+{
+    if (!_healthStore || !type) {
+        return NO;
+    }
+    
+    if (!label) {
+        label = @"Undefined";
+    }
+    
+    NSString *message = nil;
+    
+    // authorizationStatusForType: only works for write types.
+    // For read types, if sharing was deined, it will simple returns as no data.
+    switch ([_healthStore authorizationStatusForType:type]) {
+        case HKAuthorizationStatusNotDetermined:
+            message = @"Not Determined";
+            break;
+        case HKAuthorizationStatusSharingDenied:
+            message = @"Sharing Denied";
+            break;
+        case HKAuthorizationStatusSharingAuthorized:
+            return YES;
+        default:
+            break;
+    }
+    message = [NSString stringWithFormat:@"%@: %@",
+               label,
+               message];
+    
+    [self addText:message];
+    
+    return NO;
+}
+
+#pragma mark Birthday
+
+- (void)getBirthday
+{
+    NSError *error;
+    NSDateComponents *birthdayComponents
+    = [_healthStore dateOfBirthComponentsWithError:&error];
+    
+    if (!error) {
+        
+        if (birthdayComponents) {
+            NSDate *birthday = [birthdayComponents date];
+            if (birthday) {
+                NSDateFormatter *dateFormatter = [NSDateFormatter new];
+                [dateFormatter setTimeZone:[NSTimeZone systemTimeZone]];
+                [dateFormatter setDateFormat:@"'Birthday: 'yyyy-MM-dd"];
+                [self addText:[dateFormatter stringFromDate:birthday]];
+            }
+        } else {
+            [self addText:@"Birthday: Not Set (or Sharing Denied)"];
+        }
+        
+    } else {
+        [self addText:[NSString stringWithFormat:@"Birthday: %@",
+                       [error localizedDescription]]];
+    }
+}
+
+#pragma mark Bio Sex
+
+- (void)getBiologicalSex
+{
+    NSError *error;
+    HKBiologicalSexObject *bioSex
+    = [_healthStore biologicalSexWithError:&error];
+    
+    NSString *message = nil;
+    
+    if (!error) {
+        switch (bioSex.biologicalSex) {
+            case HKBiologicalSexNotSet:
+                message = @"Not Set (or Sharing Denied)";
+                break;
+            case HKBiologicalSexFemale:
+                message = @"Female";
+                break;
+            case HKBiologicalSexMale:
+                message = @"Male";
+                break;
+            case HKBiologicalSexOther:
+                message = @"Other";
+                break;
+            default:
+                message = @"Error";
+                break;
+        }
+        
+        if (message) {
+            message = [NSString stringWithFormat:@"Sex: %@",
+                       message];
+            [self addText:message];
+        }
+        
+    } else {
+        [self addText:[NSString stringWithFormat:@"Sex: %@",
+                       [error localizedDescription]]];
+    }
+}
+
+#pragma mark Height
+
+- (void)getHeight
+{
+//    NSCalendar *calendar = [NSCalendar currentCalendar];
+//    NSDateComponents *components =
+//    [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay
+//                fromDate:[NSDate date]];
+//    NSDate *startDate = [calendar dateFromComponents:components];
+//    NSDate *endDate = [calendar dateByAddingUnit:NSCalendarUnitDay
+//                                           value:1
+//                                          toDate:startDate
+//                                         options:0];
+//    NSPredicate *predicate =
+//    [HKQuery predicateForSamplesWithStartDate:startDate
+//                                      endDate:endDate
+//                                      options:HKQueryOptionNone];
+    
+    [self getSampleQueryWithTypeIdentifier:HKQuantityTypeIdentifierHeight
+                                     label:@"Height"
+                                 predicate:nil
+                                     limit:HKObjectQueryNoLimit
+                                 ascending:NO];
+}
+
+#pragma mark Weight
+
+- (void)getWeight
+{
+    [self getSampleQueryWithTypeIdentifier:HKQuantityTypeIdentifierBodyMass
+                                     label:@"Weight"
+                                 predicate:nil
+                                     limit:HKObjectQueryNoLimit
+                                 ascending:NO];
+}
+
+#pragma mark Step Count
+
+- (void)getStepCount
+{
+    [self getSampleQueryWithTypeIdentifier:HKQuantityTypeIdentifierStepCount
+                                     label:@"Step Count (Last 10)"
+                                 predicate:nil
+                                     limit:10
+                                 ascending:NO];
+}
+
+#pragma mark Heart Rate
+
+- (void)getHeartRate
+{
+    [self getSampleQueryWithTypeIdentifier:HKQuantityTypeIdentifierHeartRate
+                                     label:@"Heart Rate (Last 10)"
+                                 predicate:nil
+                                     limit:10
+                                 ascending:NO];
+}
+
+#pragma mark - Utils
+
+- (void)getSampleQueryWithTypeIdentifier:(HKQuantityTypeIdentifier)identifier
+                                   label:(NSString *)label
+                               predicate:(NSPredicate *)predicate
+                                   limit:(NSUInteger)limit
+                               ascending:(BOOL)ascending
+{
+    if (!_healthStore || !identifier) {
+        return;
+    }
+    
+    if (!label) {
+        label = identifier;
+    }
+    
+    NSSortDescriptor *dateDescriptor =
+    [[NSSortDescriptor alloc] initWithKey:HKSampleSortIdentifierStartDate
+                                ascending:ascending];
+    
+    HKSampleType *sampleType =
+    [HKSampleType quantityTypeForIdentifier:identifier];
+    
+    HKSampleQuery *query =
+    [[HKSampleQuery alloc] initWithSampleType:sampleType
+                                    predicate:predicate
+                                        limit:limit
+                              sortDescriptors:@[dateDescriptor]
+                               resultsHandler:^(HKSampleQuery *query,
+                                                NSArray *results,
+                                                NSError *error)
+     {
+         if (error) {
+             NSLog(@"%@: %@",
+                   label,
+                   [error localizedDescription]);
+             [self addText:[NSString stringWithFormat:@"%@: %@",
+                            label,
+                            [error localizedDescription]]];
+             return;
+         }
+         
+         if (results) {
+             if (results.count) {
+                 NSMutableArray *array = [NSMutableArray new];
+                 
+                 NSDateFormatter *dateFormatter = [NSDateFormatter new];
+                 [dateFormatter setTimeZone:[NSTimeZone systemTimeZone]];
+                 [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+                 
+                 for (HKQuantitySample *sample in results) {
+                     [array addObject:[NSString stringWithFormat:@"> %@ (%@ - %@)",
+                                       sample.quantity,
+                                       [dateFormatter stringFromDate:sample.startDate],
+                                       [dateFormatter stringFromDate:sample.endDate]]];
+                 }
+                 NSString *message = [NSString stringWithFormat:@"%@:\n%@",
+                                      label,
+                                      [array componentsJoinedByString:@",\n"]];
+                 [self addText:message];
+             } else {
+                 [self addText:[NSString stringWithFormat:@"%@: No Data",
+                                label]];
+             }
+             
+         }
+     }];
+    
+    [_healthStore executeQuery:query];
+}
+
+#pragma mark - Text View
+
+- (void)clearTextView
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        _textView.text = @"";
+    });
+}
+
+- (void)addText:(NSString *)string
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        _textView.text = [NSString stringWithFormat:@"%@\n%@",
+                          _textView.text,
+                          string];
+    });
+}
+
+#pragma mark - IBActions
+
+- (IBAction)getHealthData:(id)sender
+{
+    [self getHealthKit];
+}
 
 @end
